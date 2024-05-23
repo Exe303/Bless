17a217b2781fe1b960f27213d28b348f74b04e98
Exe303 <127712981+Exe303@users.noreply.github.com>
2024-05-23 23:00:03 +0700

    Update mx-ray.sh

--- V2ray/mx-ray.sh
+++ V2ray/mx-ray.sh
@@ -106,7 +106,7 @@ cat > /etc/v2ray/config.json <<-EOF
           ]
         },
         "wsSettings": {
-          "path": "/servlets/mms",
+          "path": "/mifacell",
           "headers": {
             "Host": ""
           }
@@ -195,7 +195,7 @@ cat > /etc/v2ray/none.json <<-EOF
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/servlets/mms",
+          "path": "/mifacell",
           "headers": {
             "Host": ""
           }
@@ -293,7 +293,7 @@ cat > /etc/v2ray/vless.json <<-EOF
           ]
         },
         "wsSettings": {
-          "path": "/servlets/mms",
+          "path": "/mifacell",
           "headers": {
             "Host": ""
           }
@@ -381,7 +381,7 @@ cat > /etc/v2ray/vnone.json <<-EOF
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/servlets/mms",
+          "path": "/mifacell",
           "headers": {
             "Host": ""
           }
